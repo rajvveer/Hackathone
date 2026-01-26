b63700df927174b93c74563b9d29112bc934ea66
unknown <rajveershekhawat626@gmail.com>
2026-01-26 14:21:07 +0530

    added more controlelrs

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -1,10 +1,12 @@
-const { pool } = require('../config/db');
+// IMPORT THE MODEL
+const Task = require('../models/taskModel');
 
 // Get Tasks
 const getTasks = async (req, res) => {
   try {
-    const tasks = await pool.query('SELECT * FROM tasks WHERE user_id = $1 ORDER BY id ASC', [req.user.id]);
-    res.json(tasks.rows);
+    // Use Model
+    const tasks = await Task.findAllByUser(req.user.id);
+    res.json(tasks);
   } catch (err) {
     res.status(500).send('Server Error');
   }
@@ -13,7 +15,8 @@ const getTasks = async (req, res) => {
 // Complete Task
 const completeTask = async (req, res) => {
   try {
-    await pool.query("UPDATE tasks SET status = 'completed' WHERE id = $1 AND user_id = $2", [req.params.id, req.user.id]);
+    // Use Model
+    await Task.markComplete(req.params.id, req.user.id);
     res.json({ msg: "Task Completed" });
   } catch (err) {
     res.status(500).send('Server Error');
